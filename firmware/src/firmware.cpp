bfb56a65a2bcee4b2add37031d21c63448a98eda
Lucas Pleß <lucas@xleave.de>
2018-12-21 12:40:40 +0100

    first rough base of a firmware

--- firmware/src/firmware.cpp
+++ firmware/src/firmware.cpp
@@ -1,43 +1,113 @@
 #include <Arduino.h>
+#include "InputDebounce.h"
 
-#define IN_1A   5
-#define IN_2A   4
-#define EN_A    2
-#define EN_B    A4
+#define LED     13
+#define MOTOR_IN_1    6
+#define MOTOR_IN_2    5
+#define MOTOR_EN_A    3
+#define MOTOR_EN_B    A5
+#define SWITCH_FRONT    7
+#define SWITCH_BACK     8
+#define TICK_DELAY_MS   20
+#define BUTTON_DEBOUNCE_DELAY 5 // [ms]
+
+#define DUTY_MIN    0
+#define DUTY_MAX    50      //TODO: figure out maximum sensible speed
+
+
+enum state {
+    IDLE,
+    RAMP_UP,
+    MOVE,
+    RAMP_DOWN,
+} cur_state = IDLE;
+
+enum direction {
+    FORWARD,
+    REVERSE,
+    BRAKE
+} cur_direction = BRAKE;
+byte cur_duty = 0;
+static InputDebounce input_front;
+static InputDebounce input_back;
+
 
 void setup() {
 
-    pinMode(13, OUTPUT);
+    pinMode(LED, OUTPUT);
 
-    pinMode(IN_1A, OUTPUT);
-    pinMode(IN_2A, OUTPUT);
-    pinMode(EN_A, OUTPUT);
-    pinMode(EN_B, OUTPUT);
+    pinMode(MOTOR_IN_1, OUTPUT);
+    pinMode(MOTOR_IN_2, OUTPUT);
+    pinMode(MOTOR_EN_A, OUTPUT);
+    pinMode(MOTOR_EN_B, OUTPUT);
 
-    digitalWrite(IN_1A, LOW);
-    digitalWrite(IN_2A, LOW);
-    digitalWrite(EN_A, HIGH);
-    digitalWrite(EN_B, HIGH);
+    input_front.setup(SWITCH_FRONT, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
+    input_back.setup(SWITCH_BACK, BUTTON_DEBOUNCE_DELAY, InputDebounce::PIM_INT_PULL_UP_RES);
 
+    digitalWrite(MOTOR_IN_1, LOW);
+    digitalWrite(MOTOR_IN_2, LOW);
+    digitalWrite(MOTOR_EN_A, HIGH);
+    digitalWrite(MOTOR_EN_B, HIGH);
+
+    // start slowly moving
 
 }
 
 
 void loop() {
 
+    static long tick_millis = millis();
+    long now_millis = millis();
 
-    digitalWrite(IN_1A, HIGH);
-    digitalWrite(IN_2A, LOW);
+    unsigned int front_on_time = input_front.process(now);
+    unsigned int back_on_time = input_back.process(now);
 
-    digitalWrite(13, HIGH);
 
-    delay(200);
+    if(front_on_time) {
+        if(cur_state == MOVE && cur_direction == FORWARD) {
+            cur_state = RAMP_DOWN;
+        }
+    }
+    if(back_on_time) {
+        if(cur_state == MOVE && cur_direction == REVERSE) {
+            cur_state == RAMP_DOWN;
+        }
+    }
 
-    digitalWrite(IN_1A, LOW);
-    digitalWrite(IN_2A, HIGH);
 
-    digitalWrite(13, LOW);
-    delay(200);
+    if(now_millis - tick_millis > TICK_DELAY_MS) {
 
+
+
+        if(cur_state == RAMP_UP) {
+            if(cur_duty < DUTY_MAX) {
+                cur_duty++;
+            } else {
+                cur_state = MOVE;
+            }
+        } else if(cur_state == RAMP_DOWN) {
+            if(cur_duty > DUTY_MIN) {
+                cur_duty--;
+            } else {
+                cur_state = MOVE;
+            }
+        }
+
+
+
+
+        if(cur_direction == BRAKE) {
+            analogWrite(MOTOR_IN_1, DUTY_MIN);
+            analogWrite(MOTOR_IN_2, DUTY_MIN);
+        } else if(cur_direction == FORWARD) {
+            analogWrite(MOTOR_IN_1, cur_duty);
+            analogWrite(MOTOR_IN_2, DUTY_MIN);
+        } else if(cur_direction == REVERSE) {
+            analogWrite(MOTOR_IN_1, DUTY_MIN);
+            analogWrite(MOTOR_IN_2, cur_duty);
+        }
+
+        tick_millis = now_millis;
+    }
 
 }
