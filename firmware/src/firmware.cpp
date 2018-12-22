20936e0f8b438ad3d006b74d77c17d9c494778af
Lucas Pleß <lucas@xleave.de>
2018-12-22 22:28:58 +0100

    start adding timeout and overcurrent fatures/states

--- firmware/src/firmware.cpp
+++ firmware/src/firmware.cpp
@@ -6,6 +6,7 @@
 #define MOTOR_IN_2              5
 #define MOTOR_EN_A              3
 #define MOTOR_EN_B              A5
+#define PIN_CURRENT             A1
 #define SWITCH_FRONT            7
 #define SWITCH_BACK             8
 #define TICK_DELAY_MS           200
@@ -14,6 +15,7 @@
 
 #define DUTY_MIN                0
 #define DUTY_MAX                12      //TODO: figure out maximum sensible speed
+#define CURRENT_MAX             5000    // in mA
 
 #define DEBUG
 
@@ -23,6 +25,8 @@ enum state {
     RAMP_UP,
     MOVE,
     RAMP_DOWN,
+    TIMEOUT,
+    OVERCURRENT,
 } cur_state = IDLE;
 
 enum direction {
@@ -33,11 +37,28 @@ enum direction {
 
 direction next_direction = FORWARD;
 byte cur_duty = 0;
+int16_t cur_current = 0; // in mA  10A = 10000
 static InputDebounce input_front;
 static InputDebounce input_back;
 static long tick_millis;
 static long switch_millis;
 
+int16_t sample_current_sensor() {
+    int16_t val;
+    long temp = 0;
+
+    for (int i = 0; i < 10; ++i) {
+        temp += analogRead(PIN_CURRENT);
+        delay(1);
+    }
+
+    temp /= 10;
+
+    // Allegro ACS712 ELC 30A = 66mV/A or 0,066mV per mA
+    //val = temp * (5.0 / 1023.0);
+
+    return val;
+}
 
 void setup() {
 
@@ -105,7 +126,9 @@ void loop() {
 
 #ifdef DEBUG
         Serial.print("state: ");
-        Serial.print(cur_state == IDLE ? "IDLE" : (cur_state == RAMP_UP ? "RAMP_UP" : (cur_state == RAMP_DOWN ? "RAMP_DOWN": "MOVE")));
+        Serial.print(cur_state == IDLE ? "IDLE" : (cur_state == RAMP_UP ? "RAMP_UP" :
+            (cur_state == RAMP_DOWN ? "RAMP_DOWN": (cur_state == MOVE ? "MOVE" :
+            (cur_state == TIMEOUT ? "TIMEOUT" : (cur_state == OVERCURRENT ? "OVERCURRENT" : "UNKNOWN"))))));
         Serial.print(" duty: ");
         Serial.print(cur_duty);
         Serial.print(" direction: ");
